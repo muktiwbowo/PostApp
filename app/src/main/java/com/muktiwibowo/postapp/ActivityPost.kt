cf5fa73479d1a6ce429f025c1aa955437312ce01
Mukti Wibowo <muktiwbowo@gmail.com>
2022-11-09 21:29:40 +0700

    add view binding

--- app/src/main/java/com/muktiwibowo/postapp/ActivityPost.kt
+++ app/src/main/java/com/muktiwibowo/postapp/ActivityPost.kt
@@ -2,10 +2,12 @@ package com.muktiwibowo.postapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muktiwibowo.postapp.databinding.ActivityPostBinding
 
 class ActivityPost : AppCompatActivity() {
+    private lateinit var binding: ActivityPostBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post)
+        setContentView(binding.root)
     }
 }
